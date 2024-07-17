c1db517747473ad657e4bc26e25cb270485768f4
devpavanello <enzo@pavanello.biz>
2024-07-17 10:23:08 -0300

    Second commit

--- app.js
+++ app.js
@@ -4,9 +4,26 @@ alert('Boas vindas ao jogo do número secreto!');
 //Criação de variaveis
 let numeroSecreto = 29;
 
-//Alerta com campo para digitação do usuario
-let chute = prompt('Escolha um número entre 1 e 30');
+//Apresentacao do numero secreto no console 
+console.log(numeroSecreto);
 
-if (numeroSecreto == chute){
-    console.log('Isso aí! Você descobriu o numero secreto (5)');
+//Alerta com campo para digitação do usuario
+let chute
+
+while (chute != numeroSecreto) {
+
+    chute = prompt('Escolha um número entre 1 e 30');
+
+    //Realizando uma comparaçao entre o numero secreto e chute
+    if (numeroSecreto == chute) {
+        //Utilizaçao de (``) = Template String
+        //Para realizar uma concatenaçao utiliza ($) + {VARIAVEL}
+        alert(`Isso aí! Você descobriu o numero secreto ${numeroSecreto}`);
+    } else {
+        if (numeroSecreto > chute) {
+            alert(`O número secreto é maior que ${chute}`);
+        } else {
+            alert(`O número secreto é menor que ${chute}`);
+        }
+    }
 }
